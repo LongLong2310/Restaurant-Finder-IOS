6182775c6a0ce4c4846da0908bfef31d5a71b565
LongLong0606 <goldsky284@gmail.com>
2023-07-24 11:46:10 +0700

    Initial Commit

---
+++ restaurant finder/restaurant_finderApp.swift
@@ -0,0 +1,17 @@
+//
+//  restaurant_finderApp.swift
+//  restaurant finder
+//
+//  Created by Phạm Long on 24/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct restaurant_finderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
